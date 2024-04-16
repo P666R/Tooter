710e8a00a4d1f6d533e890a7a9c0dd83ae647ac3
PRoy <sam469roger@gmail.com>
2024-04-16 17:52:22 +0530

    initial deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
@@ -9,7 +10,7 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
-const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +20,14 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, async () => {
   await connectToMongoDB();
