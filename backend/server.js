a3439cbf607ab35976b4eb063de6611606c7e72d
PRoy <sam469roger@gmail.com>
2024-04-16 07:59:05 +0530

    frontend setup socket.io-client

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
+const PORT = process.env.PORT || 5000;
+
 dotenv.config();
 
 app.use(express.json());
@@ -17,8 +19,6 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
-
-const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, async () => {
   await connectToMongoDB();
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,7 +5,6 @@ import express from 'express';
 const app = express();
 
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: ['http://localhost:3000'],
@@ -13,11 +12,22 @@ const io = new Server(server, {
   },
 });
 
+const userSocketMap = {}; // { userId: socketId }
+
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
 
+  const userId = socket.handshake.query.userId;
+  if (userId !== undefined) {
+    userSocketMap[userId] = socket.id;
+  }
+  io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
   socket.on('disconnect', () => {
     console.log('user disconnected', socket.id);
+
+    delete userSocketMap[userId];
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
   });
 });
 
