37c5e26083623273c2df1a91dbf9991b96808bf9
PRoy <sam469roger@gmail.com>
2024-04-16 13:46:21 +0530

    implemented emoji picker

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import { BsSend } from 'react-icons/bs';
 import useSendMessage from '../../hooks/useSendMessage';
+import { BsEmojiSmileFill } from 'react-icons/bs';
+import EmojiPicker from 'emoji-picker-react';
 
 function MessageInput() {
   const [message, setMessage] = useState('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const { loading, sendMessage } = useSendMessage();
 
   async function handleSubmit(e) {
@@ -14,26 +17,41 @@ function MessageInput() {
     setMessage('');
   }
 
+  function handleEmojiClick(emoji) {
+    setMessage((curr) => curr + emoji.emoji);
+    setShowEmojiPicker(false);
+  }
+
   return (
-    <form className="px-4 my-3" onSubmit={handleSubmit}>
-      <div className="w-full relative">
-        <input
-          type="text"
-          className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
-          placeholder="Send a message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+    <form className="px-4 my-3 relative" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
+        placeholder="Send a message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        type="button"
+        className="absolute inset-y-0 right-10 flex items-center pe-4"
+        onClick={() => setShowEmojiPicker((curr) => !curr)}
+      >
+        <BsEmojiSmileFill className="w-5 h-5 text-white cursor-pointer" />
+      </button>
+      <button
+        type="submit"
+        className="absolute inset-y-0 right-0 flex items-center pe-6"
+      >
+        {loading ? <div className="loading loading-spinner"></div> : <BsSend />}
+      </button>
+      <div className="absolute bottom-14 right-12 opacity-90">
+        <EmojiPicker
+          open={showEmojiPicker}
+          theme="dark"
+          lazyLoadEmojis
+          height={380}
+          onEmojiClick={handleEmojiClick}
         />
-        <button
-          type="submit"
-          className="absolute inset-y-0 end-0 flex items-center pe-3"
-        >
-          {loading ? (
-            <div className="loading loading-spinner"></div>
-          ) : (
-            <BsSend />
-          )}
-        </button>
       </div>
     </form>
   );
